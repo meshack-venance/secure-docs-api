a380cf8872310112bf6717684d87af9fe9a18434
meshack-venance <meshackvenance99@gmail.com>
2026-06-15 14:22:05 +0300

    feat: register documents in admin

--- documents/admin.py
+++ documents/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from documents.models import Document
+
+
+@admin.register(Document)
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "status",
+        "verification_code",
+        "uploaded_by",
+        "created_at",
+        "updated_at",
+    )
+    list_filter = ("status", "created_at", "updated_at")
+    search_fields = ("title", "description", "verification_code", "uploaded_by__email")
+    readonly_fields = ("verification_code", "created_at", "updated_at")
