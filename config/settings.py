18984900a7594821bc66872b53ae6a11bb6f379d
meshack-venance <meshackvenance99@gmail.com>
2026-06-15 13:29:21 +0300

    feat: configure jwt authentication

--- config/settings.py
+++ config/settings.py
@@ -74,6 +74,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 AUTH_USER_MODEL = 'accounts.User'
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+}
+
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
