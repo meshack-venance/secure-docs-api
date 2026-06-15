d5f1b863401278a833c8236e4b403f767d8b39c0
meshack-venance <meshackvenance99@gmail.com>
2026-06-15 13:29:55 +0300

    feat: add auth serializers

---
+++ accounts/serializers.py
@@ -0,0 +1,39 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+
+User = get_user_model()
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            "id",
+            "email",
+            "first_name",
+            "last_name",
+            "role",
+            "is_active",
+            "date_joined",
+        )
+        read_only_fields = ("id", "role", "is_active", "date_joined")
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=8)
+
+    class Meta:
+        model = User
+        fields = (
+            "id",
+            "email",
+            "first_name",
+            "last_name",
+            "password",
+        )
+        read_only_fields = ("id",)
+
+    def create(self, validated_data):
+        password = validated_data.pop("password")
+        return User.objects.create_user(password=password, **validated_data)
